4d649636c569c0fc5f032ceb111a61e96d4c7d85
habib-zinelabidine <habib.zinelabidine@gmail.com>
2023-12-26 21:05:25 +0100

    adding card

--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -1,18 +1,24 @@
-import style from './TopicCard.module.css'
+import style from "./TopicCard.module.css";
 
 export default function TopicCard() {
   return (
     <div className={style.topic_container}>
-      <img
-        alt="topic image"
-        src="https://cc-west-blog-assets.s3.amazonaws.com/uploads/2016/09/write-any-topic.jpg"
-      />
-      <h2>Topics</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ad
-        corporis voluptatibus laboriosam sit quas, eius quia rerum quasi.
-        Placeat repudiandae eveniet rem voluptates inventore!
-      </p>
+      <div className={style.topic_image}>
+        <img
+          alt="topic image"
+          src="https://cc-west-blog-assets.s3.amazonaws.com/uploads/2016/09/write-any-topic.jpg"
+        />
+      </div>
+      <div className={style.card_content}>
+        <h2>Topics</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          ad corporis voluptatibus laboriosam sit quas
+        </p>
+      </div>
+      <div className={style.avatars}>
+        <img src="https://cc-west-blog-assets.s3.amazonaws.com/uploads/2016/09/write-any-topic.jpg" />
+      </div>
     </div>
   );
 }
